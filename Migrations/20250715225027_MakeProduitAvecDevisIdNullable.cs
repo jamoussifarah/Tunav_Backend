0b1fbc9eaeaa74ac307cb956a3643d3df5f1c0e2
jamoussifarah <jamoussifarah03@gmail.com>
2025-07-16 09:58:31 +0100

    devis-changement

---
+++ Migrations/20250715225027_MakeProduitAvecDevisIdNullable.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TunavBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeProduitAvecDevisIdNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Devis_ProduitsAvecDevis_ProduitAvecDevisId",
+                table: "Devis");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProduitAvecDevisId",
+                table: "Devis",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Devis_ProduitsAvecDevis_ProduitAvecDevisId",
+                table: "Devis",
+                column: "ProduitAvecDevisId",
+                principalTable: "ProduitsAvecDevis",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Devis_ProduitsAvecDevis_ProduitAvecDevisId",
+                table: "Devis");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProduitAvecDevisId",
+                table: "Devis",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Devis_ProduitsAvecDevis_ProduitAvecDevisId",
+                table: "Devis",
+                column: "ProduitAvecDevisId",
+                principalTable: "ProduitsAvecDevis",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
